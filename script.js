56e19aab70bcb214b8c5888f49a39497661772bb
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-21 11:44:46 +0530

    in progress

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ videocontainer.addEventListener('mouseleave', () => {
     videocircle.style.transition = "all 0.3s";
 
 })
+let fourthlines = document.querySelectorAll('.fourthline');
+fourthlines.forEach(fourthline => {
+    fourthline.addEventListener('mouseenter', () => {
+        let greenarrow = fourthline.nextElementSibling;
+        greenarrow.style.marginLeft = "18px";
+        greenarrow.style.transition = "all 0.3s";
+    });
+
+    fourthline.addEventListener('mouseleave', () => {
+        let greenarrow = fourthline.nextElementSibling;
+        greenarrow.style.marginLeft = "10px";
+        greenarrow.style.transition = "all 0.3s";
+    });
+});
